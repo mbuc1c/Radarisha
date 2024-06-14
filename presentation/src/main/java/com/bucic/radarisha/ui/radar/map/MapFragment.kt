2af59d58ad1f40ad73c666aa06124a841eda1f4e
mbuc1c <you@example.com>
2024-06-14 15:32:27 +0200

    navigation-between-activities

---
+++ presentation/src/main/java/com/bucic/radarisha/ui/radar/map/MapFragment.kt
@@ -0,0 +1,41 @@
+package com.bucic.radarisha.ui.radar.map
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.bucic.radarisha.R
+import com.bucic.radarisha.databinding.FragmentMapBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MapFragment : Fragment() {
+
+
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.extendedFab.setOnClickListener {
+            findNavController().navigate(R.id.action_MapFragment_to_RadarCreateFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
