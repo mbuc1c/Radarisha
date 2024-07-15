f7c3c8f0c80f1c70d58bc7f7336c7e94c346e2c8
mbuc1c <you@example.com>
2024-07-15 14:32:25 +0200

    show camera speed picker when needed

--- presentation/src/main/java/com/bucic/radarisha/ui/radar/create/RadarCreateFragment.kt
+++ presentation/src/main/java/com/bucic/radarisha/ui/radar/create/RadarCreateFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.viewModels
 import com.bucic.domain.util.RadarType
 import com.bucic.radarisha.R
@@ -20,40 +22,71 @@ class RadarCreateFragment : Fragment() {
     private var _binding: FragmentRadarCreateBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var initialConstraint: ConstraintSet
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentRadarCreateBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initialConstraint = ConstraintSet()
+        initialConstraint.clone(binding.constraintLayout)
+
         val radarTypesEnum = RadarType.entries.map { it.displayName }
         val radarTypeArrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, radarTypesEnum)
-        val typeAutocompleteTextView = binding.typeAutoCompleteTextView
-        typeAutocompleteTextView.setAdapter(radarTypeArrayAdapter)
+        binding.typeAutoCompleteTextView.setAdapter(radarTypeArrayAdapter)
 
         val speedValues = resources.getIntArray(R.array.radar_speed)
         val speedArrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, speedValues.map { it.toString() })
-        val speedAutocompleteTextView = binding.speedAutoCompleteTextView
-        speedAutocompleteTextView.setAdapter(speedArrayAdapter)
+        binding.speedAutoCompleteTextView.setAdapter(speedArrayAdapter)
+
+        binding.typeAutoCompleteTextView.setOnItemClickListener { parent, _, position, _ ->
+            val selectedType = parent.getItemAtPosition(position) as String
+            if (selectedType != RadarType.SPEED_CAMERA.displayName) {
+                removeMenuSpeedAndMoveLocation()
+            } else {
+                revertToOriginalConstraints()
+            }
+        }
 
         binding.createRadarButton.setOnClickListener {
+            // temporary
             Toast.makeText(
                 requireContext(),
-                "Selected type: ${typeAutocompleteTextView.text} \n" +
-                        "Selected speed: ${speedAutocompleteTextView.text}",
+                "Selected type: ${binding.typeAutoCompleteTextView.text} \n" +
+                        "Selected speed: ${binding.speedAutoCompleteTextView.text}",
                 Toast.LENGTH_SHORT
             ).show()
         }
 
         // TODO: get current location address
-//        binding.tvAddress.text = "Address of current location"
+        binding.tvAddress.text = "placeholder"
+
+        removeMenuSpeedAndMoveLocation()
+    }
+
+    private fun revertToOriginalConstraints() {
+        binding.constraintLayout.addView(binding.menuSpeed)
+        initialConstraint.applyTo(binding.constraintLayout)
+    }
+
+    private fun removeMenuSpeedAndMoveLocation() {
+        with(binding) {
+            constraintLayout.removeView(menuSpeed)
+
+            val constraintSet = ConstraintSet()
+            with(constraintSet) {
+                clone(constraintLayout)
+                connect(tvLocation.id, ConstraintSet.TOP, menuRadarType.id, ConstraintSet.BOTTOM)
+                applyTo(constraintLayout)
+            }
+        }
     }
 
     override fun onDestroyView() {
