58f069cc74f18bde336d5cb3b19758f0ef4138da
mbuc1c <you@example.com>
2024-08-10 18:27:33 +0200

    splash screen

---
+++ presentation/src/main/java/com/bucic/radarisha/ui/splash/SplashActivity.kt
@@ -0,0 +1,46 @@
+package com.bucic.radarisha.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.bucic.radarisha.R
+import com.bucic.radarisha.ui.auth.AuthActivity
+import com.bucic.radarisha.ui.radar.RadarActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import com.bucic.domain.util.Result
+import kotlinx.coroutines.flow.collect
+
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+
+    private val viewModel: SplashViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.SplashTheme)
+        super.onCreate(savedInstanceState)
+
+        getCurrentUser()
+
+        lifecycleScope.launch {
+            viewModel.isLoggedIn.collectLatest { isLoggedIn ->
+                if (isLoggedIn) {
+                    startActivity(Intent(this@SplashActivity, RadarActivity::class.java))
+                    finish()
+                } else {
+                    startActivity(Intent(this@SplashActivity, AuthActivity::class.java))
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun getCurrentUser() {
+        viewModel.getCurrentUser()
+    }
+}
